65085b1e300f24204533c43f524572e51df1ae3c
lordrizkyfaisal <96189968+lordrizkyfaisal@users.noreply.github.com>
2021-12-20 20:19:09 +0700

    Update and rename shop/allpayment.js to toko/allpayment.js

---
+++ toko/allpayment.js
@@ -0,0 +1,21 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*E - MONEY* 
+
+1. Gopay
+ㅁ 0882 1337 1423 
+
+Atas nama Kiki2323
+
+* PULSA*
+
+1. SMARTFREN
+ㅁ 0888 0949 0679
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
